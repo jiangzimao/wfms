8717fef288abbdbd7c76f29cba20b6293f2a4236
jiangzimao <jiangzimao@126.com>
2015-12-31 10:28:31 +0800

    添加domain模块

--- web/src/main/java/org/jqiaofu/wfms/web/WebApplication.java
+++ web/src/main/java/org/jqiaofu/wfms/web/WebApplication.java
@@ -1,13 +1,14 @@
-package org.jqiaofu.wfms.web.config;
+package org.jqiaofu.wfms.web;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration("org.jqiaofu.wfms")
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan("org.jqiaofu.wfms")
 public class WebApplication {
 
     public static void main(String[] args) {
